ea049b9db26d4288e59258b9e64fc7a30cfecc6e
adriantwardy <adriantwardawski@gmail.com>
2022-01-16 11:30:04 +0100

    Seeder

--- Seeder.cs
+++ Seeder.cs
@@ -18,21 +18,35 @@ namespace MyWallet
     {
         private readonly ApplicationDbContext _dbContext;
         private readonly RoleManager<IdentityRole> _roleManager;
-        public Seeder(ApplicationDbContext dbContext, RoleManager<IdentityRole> roleManager)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public Seeder(ApplicationDbContext dbContext, RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
             _dbContext = dbContext;
             _roleManager = roleManager;
+            _userManager = userManager;
         }
             public void Seed()
             {
-                if (!_dbContext.Roles.Any())
+                if (_dbContext.Roles.Any(r => r.Name == Utility.Helper.Admin)) return;
                 {
                     _roleManager.CreateAsync(new IdentityRole(Helper.Admin)).GetAwaiter().GetResult();
+                    _roleManager.CreateAsync(new IdentityRole(Helper.Manager)).GetAwaiter().GetResult();
                     _roleManager.CreateAsync(new IdentityRole(Helper.User)).GetAwaiter().GetResult();
                 }
+
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "admin@gmail.com",
+                    Email = "admin@gmail.com",
+                    Name = "Admin",
+                    EmailConfirmed = true
+                }, "Admin123.").GetAwaiter().GetResult();
+
+                ApplicationUser user = _dbContext.Users.FirstOrDefault(u => u.Email == "admin@gmail.com");
+                _userManager.AddToRoleAsync(user, Helper.Admin).GetAwaiter().GetResult();
+
             }
-           
-        
+   
     }
 
    
--- Utility/Helper.cs
+++ Utility/Helper.cs
@@ -10,14 +10,25 @@ namespace MyWallet.Utility
     {
         public static string Admin = "Admin";
         public static string User = "User";
+        public static string Manager = "Manager";
 
-        public static List<SelectListItem> GetRolesForDropDown()
+        public static List<SelectListItem> GetRolesForDropDown(bool isAdmin)
         {
-            return new List<SelectListItem>
+            if (isAdmin)
             {
-                new SelectListItem{Value = Helper.Admin, Text = Helper.Admin},
-                new SelectListItem{Value = Helper.User, Text = Helper.User}
-            };
+                return new List<SelectListItem>
+                {
+                    new SelectListItem { Value = Helper.Admin, Text = Helper.Admin }
+                };
+            }
+            else
+            {
+                return new List<SelectListItem>
+                {               
+                    new SelectListItem{Value = Helper.Manager, Text = Helper.Manager},
+                    new SelectListItem{Value = Helper.User, Text = Helper.User}
+                };
+            }
         }
     }
 }
